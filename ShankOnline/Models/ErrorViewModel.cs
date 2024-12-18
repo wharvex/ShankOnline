fc6bf438b8cff03c2518a7ca3b9914de858233bc
Tim <81175852+wharvex@users.noreply.github.com>
2024-12-18 15:17:31 -0500

    first commit

---
+++ ShankOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShankOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
